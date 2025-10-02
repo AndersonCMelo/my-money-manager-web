4214e380f1cc7c8362c10109009073314f899bf6
Anderson Melo <anderson.melo.ext@spms.min-saude.pt>
2025-10-02 17:37:23 +0100

    Add ecai widget to test in mobile device

--- src/app/app/dashboard/page.tsx
+++ src/app/app/dashboard/page.tsx
@@ -6,9 +6,34 @@ import BalanceSection from './balance-section'
 import Transactions from './transactions'
 import TransactionsTableFilter from './transactions-table-filters'
 import { PageTitle } from '@/components/ui/page-title'
+import { useEffect } from 'react'
+
+declare global {
+  interface Window {
+    EuroconsumersAI: any;
+  }
+}
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions)
+
+  useEffect(() => {
+    function loadEcaiWidget(language = 'nl') {
+      const script = document.createElement("script");
+      script.src = "https://nice-glacier-096cae503.2.azurestaticapps.net/ecai.widget.js";
+      script.async = true;
+
+      script.onload = () => {
+        if (window.EuroconsumersAI) {
+          window.EuroconsumersAI.init({ language });
+        }
+      };
+
+      document.body.appendChild(script);
+    }
+
+    loadEcaiWidget('fr');
+  }, [])
 
   return (
     <div className="flex flex-col w-full p-4 sm:p-10">
